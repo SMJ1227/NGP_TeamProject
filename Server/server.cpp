56c45afd0f7bf3025057b6802ce48b05e19c2487
SMJ1227 <112992077+SMJ1227@users.noreply.github.com>
2024-11-15 13:33:36 +0900

    serverRecvThread

---
+++ Server/server.cpp
@@ -0,0 +1,175 @@
+/*** 여기서부터 이 책의 모든 예제에서 공통으로 포함하여 사용하는 코드이다. ***/
+
+#define _CRT_SECURE_NO_WARNINGS  // 구형 C 함수 사용 시 경고 끄기
+#define _WINSOCK_DEPRECATED_NO_WARNINGS  // 구형 소켓 API 사용 시 경고 끄기
+
+#include <stdio.h>     // printf(), ...
+#include <stdlib.h>    // exit(), ...
+#include <string.h>    // strncpy(), ...
+#include <tchar.h>     // _T(), ...
+#include <winsock2.h>  // 윈속2 메인 헤더
+#include <ws2tcpip.h>  // 윈속2 확장 헤더
+
+#pragma comment(lib, "ws2_32")  // ws2_32.lib 링크
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(const char* msg) {
+  LPVOID lpMsgBuf;
+  FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+                 NULL, WSAGetLastError(),
+                 MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&lpMsgBuf, 0,
+                 NULL);
+  MessageBoxA(NULL, (const char*)lpMsgBuf, msg, MB_ICONERROR);
+  LocalFree(lpMsgBuf);
+  exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_display(const char* msg) {
+  LPVOID lpMsgBuf;
+  FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+                 NULL, WSAGetLastError(),
+                 MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&lpMsgBuf, 0,
+                 NULL);
+  printf("[%s] %s\n", msg, (char*)lpMsgBuf);
+  LocalFree(lpMsgBuf);
+}
+
+// 소켓 함수 오류 출력
+void err_display(int errcode) {
+  LPVOID lpMsgBuf;
+  FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+                 NULL, errcode, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                 (char*)&lpMsgBuf, 0, NULL);
+  printf("[오류] %s\n", (char*)lpMsgBuf);
+  LocalFree(lpMsgBuf);
+}
+
+/*** 여기까지가 이 책의 모든 예제에서 공통으로 포함하여 사용하는 코드이다. ***/
+/*** 2장 이후의 예제들은 Common.h를 포함하는 방식으로 이 코드를 사용한다.  ***/
+
+#define SERVERPORT 9000
+#define BUFSIZE 512
+
+#include <windows.h>  // windows 관련 함수 포함
+
+CRITICAL_SECTION cs;
+
+typedef struct Player {
+  int x;
+  int y;
+};
+
+typedef struct OBJECTS {
+  Player player;
+};
+
+// 클라이언트와 데이터 통신
+DWORD WINAPI RecvProcessClient(LPVOID arg) {
+  int retval;
+  SOCKET client_sock = (SOCKET)arg;
+  struct sockaddr_in clientaddr;
+  char addr[INET_ADDRSTRLEN];
+  int addrlen;
+  char buf[BUFSIZE + 1];
+
+  // 클라이언트 정보 얻기
+  addrlen = sizeof(clientaddr);
+  getpeername(client_sock, (struct sockaddr*)&clientaddr, &addrlen);
+  inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+
+  EnterCriticalSection(&cs);
+  printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n", addr, ntohs(clientaddr.sin_port));
+  LeaveCriticalSection(&cs);
+
+  while (1) {
+    // 데이터 받기
+    retval = recv(client_sock, buf, BUFSIZE, 0);
+    if (retval == SOCKET_ERROR) {
+      err_display("recv()");
+      break;
+    } else if (retval == 0)
+      break;
+
+    // 받은 데이터 출력
+    buf[retval] = '\0';
+    EnterCriticalSection(&cs);
+    printf("[%s:%d] %s\n", addr, ntohs(clientaddr.sin_port), buf);
+    LeaveCriticalSection(&cs);
+
+    if (retval == SOCKET_ERROR) {
+      err_display("send()");
+      break;
+    }
+    Sleep(100); // 초당 받는 패킷이 너무 많다. 재울까
+  }
+
+  // 소켓 닫기
+  EnterCriticalSection(&cs);
+  closesocket(client_sock);
+  printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n", addr, ntohs(clientaddr.sin_port));
+  LeaveCriticalSection(&cs);
+  return 0;
+}
+
+int main(int argc, char* argv[]) {
+  int retval;
+  int thread_id = 0;
+
+  // 윈속 초기화
+  WSADATA wsa;
+  if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
+
+  // 소켓 생성
+  SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+
+  // bind()
+  struct sockaddr_in serveraddr;
+  memset(&serveraddr, 0, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serveraddr.sin_port = htons(SERVERPORT);
+  retval = bind(listen_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+  if (retval == SOCKET_ERROR) err_quit("bind()");
+
+  // listen()
+  retval = listen(listen_sock, SOMAXCONN);
+  if (retval == SOCKET_ERROR) err_quit("listen()");
+
+  // 데이터 통신에 사용할 변수
+  SOCKET client_sock;
+  struct sockaddr_in clientaddr;
+  int addrlen;
+  HANDLE hThread;
+  InitializeCriticalSection(&cs);
+  while (1) {
+    // accept()
+    addrlen = sizeof(clientaddr);
+    client_sock = accept(listen_sock, (struct sockaddr*)&clientaddr, &addrlen);
+    if (client_sock == INVALID_SOCKET) {
+      err_display("accept()");
+      break;
+    }
+
+    // 접속한 클라이언트 정보 출력
+    char addr[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+   // printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n", addr, ntohs(clientaddr.sin_port));
+
+    // 스레드 생성
+    hThread = CreateThread(NULL, 0, RecvProcessClient, (LPVOID)client_sock, 0, NULL);
+    if (hThread == NULL) {
+      closesocket(client_sock);
+    } else {
+      CloseHandle(hThread);
+    }
+  }
+
+  // 소켓 닫기
+  closesocket(listen_sock);
+  DeleteCriticalSection(&cs);
+  // 윈속 종료
+  WSACleanup();
+  return 0;
+}
